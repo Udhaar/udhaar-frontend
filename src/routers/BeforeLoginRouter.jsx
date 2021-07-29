ac4c44b217c327831875a0a410327b14ba701d3d
Shailesh Aanand <anaandshailu@gmail.com>
2021-07-29 14:20:05 +0530

    Add Router
    Co-authored-by: Divya Garg <divyagar@users.noreply.github.com>

---
+++ src/routers/BeforeLoginRouter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+
+export const AppRouter = () => {
+  const access_token = localStorage.getItem("access_token");
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
